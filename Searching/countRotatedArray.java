ac815673d5dc7376229c34c3915f2a31b3c867c6
Adityajs1 <aditya96937@gmail.com>
2026-01-21 14:32:02 +0530

    Update the repo

---
+++ Searching/countRotatedArray.java
@@ -0,0 +1,37 @@
+package Searching;
+
+import org.w3c.dom.css.Counter;
+
+public class countRotatedArray {
+    public static void main(String[] args) {
+      int[] arr = {4, 5, 6, 7, 0, 1, 2};
+      System.out.println(CountRotation(arr));
+    }
+
+    private static int CountRotation(int[] arr){
+           int pivot = findPivot(arr);
+           return pivot + 1;
+    }
+
+        static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length -1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(mid < end && arr[mid] > arr[mid+1]){
+                return mid;
+            }
+            if(start < mid && arr[mid] < arr[mid-1]){
+               return mid-1;
+            }
+            if(arr[start] > arr[mid]){
+                end = mid - 1;
+            }
+            else{
+               start = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ revisitConcepts/Array/reverseArray.java
@@ -0,0 +1,36 @@
+package revisitConcepts.Array;
+
+public class reverseArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        
+        System.out.println("Before: ");
+        printArray(arr);
+        
+        reverseArray(arr);  // No return value needed
+        
+        System.out.println("After: ");
+        printArray(arr);
+    }
+    
+    public static void reverseArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            
+            start++;
+            end--;
+        }
+    }
+    
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
