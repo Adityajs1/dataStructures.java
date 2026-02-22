72ff5e56b31688d159340448dddc66160920e5a0
Adityajs1 <aditya96937@gmail.com>
2026-02-22 22:56:49 +0530

    update

---
+++ String/string.java
@@ -0,0 +1,9 @@
+public class string {
+    public static void main(String[] args) {
+        String name = "Aditya";
+        System.out.println(name);
+
+        name = "Adarsh";
+        System.out.println(name);
+    }
+}
